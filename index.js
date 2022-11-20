79e93133d6f686d37ec85cf19e48e5976d865219
timothyho12321 <113851311+timothyho12321@users.noreply.github.com>
2022-11-20 15:25:59 +0800

    First phase of creating post for newcarandengine in mongo

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ async function main() {
 
 
 
-    // POST
+    // POST FIRST GENERIC ATTEMPT 
 
     app.post('/car', async function (req, res) {
 
@@ -88,7 +88,7 @@ async function main() {
             let description = req.body.description;
             let still_in_production = req.body.still_in_production;
             let cost_price = req.body.cost_price;
-           
+
             // TO FILL IN SOME MORE
 
             // handle cases where description or food is falsely
@@ -131,24 +131,156 @@ async function main() {
 
     })
 
+
+    // POST A NEW CAR WITH NEW ENGINE ATTEMPT
+    app.post('/newcarandengine', async function (req, res) {
+
+        try {
+            let name_of_model = req.body.name_of_model;
+            let year_of_launch = req.body.year_of_launch;
+            let brand = req.body.brand;
+            let type = req.body.type;
+            let seats = req.body.seats;
+            let color = req.body.color;
+            let land_terrain = req.body.land_terrain;
+            let username = req.body.username;
+            let email = req.body.email;
+            let rating = req.body.rating;
+            let description = req.body.description;
+            let still_in_production = req.body.still_in_production;
+            let cost_price = req.body.cost_price;
+
+            let top_speed = req.body.top_speed
+            let engine_power = req.body.engine_power
+            let oil_consumption = req.body.oil_consumption
+
+            // TO FILL IN SOME MORE
+
+            // handle cases where description or food is falsely
+            if (!name_of_model || !year_of_launch) {
+                // they don't end the route function
+                res.status(400);
+                res.json({
+                    'error': 'Things must be filled in'
+                })
+                return; // we must explictly return because a route
+                // can perform res.json, res.render or res.send once
+            }
+
+// INSERTED BODY VALUES PUT DO NOT ADD THE LAST THREE 
+            // {
+            //     "name_of_model": name_of_model,
+            //     "year_of_launch": year_of_launch,
+            //     "brand": brand,
+            //     "type": type,
+            //     "seats": seats,
+            //     "color": color,
+            //     "land_terrain": land_terrain,
+            //     "username": username,
+            //     "email": email,
+            //     "rating": rating,
+            //     "description": description,
+            //     "still_in_production": still_in_production,
+            //     "cost_price": cost_price,
+
+                
+            //     "engine_performance_id": 1,
+
+            //     "top_speed": top_speed,
+            //     "engine_power": engine_power,
+            //     "oil_consumption": oil_consumption
+
+            // }
+
+            // INSERT NEW ENGINE FIRST THEN GET THE ID
+
+            let engineNew = {
+                "top_speed": top_speed,
+                "engine_power": engine_power,
+                "oil_consumption": oil_consumption
+
+            }
+
+
+            const db = MongoUtil.getDB();
+            
+            const resultEngine = await db.collection("engine_performance").insertOne(engineNew);
+
+            // FIND ID OF NEW ENGINE
+
+
+            let criteria = {};
+
+
+            if (req.body.top_speed) {
+                // adding the 'description' key to the criteria object and assign req.query.description
+                // as the value
+                criteria['top_speed'] = {
+                    "$regex": req.body.top_speed,  // use regex expression search
+                    "$options": "i"  // ignore case
+                }
+            }
+
+
+            let newEngineArray = await MongoUtil.getDB().collection("engine_performance").find(criteria, {
+                // PROJECTION 
+                '_id': 1
+            }).toArray();
+
+            //TAKE OUT TOARRAY?
+
+            console.log(newEngineArray);
+
+            let carNew = {
+                "name_of_model": name_of_model,
+                "year_of_launch": year_of_launch,
+                "brand": brand,
+                "type": type,
+                "seats": seats,
+                "color": color,
+                "land_terrain": land_terrain,
+                "username": username,
+                "email": email,
+                "rating": rating,
+                "description": description,
+                "still_in_production": still_in_production,
+                "cost_price": cost_price,
+
+                // put the ID above inside here
+                "engine_performance_id": 1
+
+            }
+
+           
+            const result = await db.collection("car").insertOne(carNew);
+            res.status(200);  // set the status to 200, meaning "OK"
+            res.send(result);
+        } catch (e) {
+            console.log(e);
+            res.status(500);
+        }
+
+    })
+
+
     // UPDATE
 
     app.put('/car/:car_id', async function (req, res) {
         try {
 
-            let { 
+            let {
                 name_of_model, year_of_launch, brand
                 , type, seats, color, land_terrain
                 , username, email, rating, description
-                , still_in_production,cost_price
-                
-             } = req.body;
-              let modifiedDocument = {
+                , still_in_production, cost_price
+
+            } = req.body;
+            let modifiedDocument = {
                 name_of_model, year_of_launch, brand
                 , type, seats, color, land_terrain
                 , username, email, rating, description
-                , still_in_production,cost_price
-                
+                , still_in_production, cost_price
+
             }
 
             const result = await MongoUtil.getDB().collection('car')
